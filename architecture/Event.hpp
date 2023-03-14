af07194f58e16cb557a03852e2ba4b3974550a09
Justin Thibault <justin.thibault@epitech.eu>
2023-03-14 14:19:34 +0100

    [ADD] added Interface Game Module and added Event.hpp to manage a structur event

---
+++ architecture/Event.hpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2023
+** Event
+** File description:
+** Event
+*/
+
+#ifndef EVENT_HPP
+    #define EVENT_HPP
+
+namespace arcade {
+
+    struct Event
+    {
+        bool up;
+        bool down;
+        bool left;
+        bool right;
+        bool fire_1;
+        bool fire_2;
+        bool spe_1;
+        bool spe_2;
+        bool escape;
+    };
+};
+
+#endif
--- architecture/IGameModule.hpp
+++ architecture/IGameModule.hpp
@@ -1,4 +1,44 @@
+/*
+** EPITECH PROJECT, 2023
+** IGameModule
+** File description:
+** IGameModule
+*/
+
 #ifndef IGAMEMODULE_HPP_
     #define IGAMEMODULE_HPP_
 
-#endif /* IGAMEMODULE_HPP_ */
+    #include "Event.hpp"
+
+namespace arcade {
+
+    class IGameModule {
+
+        public:
+
+            virtual ~IGameModule() = default;
+
+            /* Handle Event */
+            virtual void event(Event) = 0;
+            /*******/
+
+            /* Process score */
+            virtual void loadHighScore() = 0;
+            virtual void updateScore() = 0;
+            virtual int getScore() = 0;
+            /*******/
+
+            virtual void quit() = 0;
+            virtual void resume() = 0;
+
+            /* Process Pause Option */
+            virtual void pause() = 0;
+            virtual bool isPaused() = 0;
+            /*******/
+
+            virtual void run() = 0;
+            virtual void restart() = 0;
+    };
+};
+
+#endif

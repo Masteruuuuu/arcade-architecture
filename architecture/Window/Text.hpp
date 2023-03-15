5f94e739824e07459d269ebaccc996f622f85c0e
Justin Thibault <justin.thibault@epitech.eu>
2023-03-15 10:27:52 +0100

    [ADD] added file color.hpp to manage values color. [UPDATE] update file text.hpp which manages different functions essential for the lib ncurse and to display texts in our game

--- architecture/Window/Text.hpp
+++ architecture/Window/Text.hpp
@@ -6,15 +6,70 @@
 */
 
 #ifndef TEXT_HPP
-#define TEXT_HPP
+    #define TEXT_HPP
 
-class Text {
-    public:
-        Text();
-        ~Text();
+    #include <string>
+    #include <vector>
+    #include "Color.hpp"
 
-    protected:
-    private:
-};
+namespace arcade {
+
+    class Text {
+
+        public:
+
+            ~Text() = default;
+
+            /* draw our text */
+            void draw();
+            /*******/
+
+            /* Postion text */
+            void setPosition(std::vector<float> pos);
+            std::vector<float> getPosition() const;
+            /*******/
+
+            /* Manage move text */
+            void move(float x, float y);
+            /*******/
+
+            /* Manage speed text */
+            void setSpeed(float speed);
+            float getSpeed() const;
+            /*******/
+
+            /* Manage rotation text */
+            void rotate(float angle);
+            float getAngle() const;
+            /*******/
+
+            /* Manage color text */
+            void setColor(Color);
+            Color getColor() const;
+            /*******/
+
+            /* Manage font text */
+            void setFont(const std::string &font);
+            std::string loadFont() const;
+            /*******/
+
+            /* Manage text. It's same for set a Sprite in SFML or other lib */
+            void setText(const std::string &text);
+            std::string getText() const;
+            /*******/
+
+            /* Manage size text */
+            void setSize(std::vector<float> size);
+            std::vector<float> getSize() const;
+            /*******/
+
+        private:
+
+            std::vector<float> size;
+            std::vector<float> pos;
+            float speed;
+            float angle;
+    };
+}
 
 #endif
